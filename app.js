752080fd046894dc9d436e42c8dfa4747d68144b
Abhinav Sharma <abhinav.sharma1993@yahoo.com>
2018-05-13 18:04:00 +0530

    added coars

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ const fetchWord=require('./BusinessLogic/fetchWords');
 const app=express();
 const port = process.env.PORT || 3000;
 
+// Add headers
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/wordlist/:number', (req, res) => {
     var number = req.params.number;
     var words=fetchWord.fetchWords(number).then((data)=>{
